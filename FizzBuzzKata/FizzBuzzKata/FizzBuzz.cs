cfd757e3633827ba02829ec7e52d801d27a5bec5
James Hollingshead <git@jameshollingshead.com>
2017-08-01 11:10:14 -0400

    FizzBuzz kata completed

--- FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzzKata/FizzBuzz.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,30 @@ namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-     
+        public string GetFizzBuzz(int p)
+        {
+            
+            if (IsDivisibleBy3(p) && IsDivisibleBy5(p))
+                return "FizzBuzz";
+            if (IsDivisibleBy3(p))
+                return "Fizz";
+            else if (IsDivisibleBy5(p))
+                return "Buzz";
+
+            return p.ToString();
+        }
+
+        private static bool IsDivisibleBy5(int p)
+        {
+            return p % 5 == 0;
+        }
+
+        private static bool IsDivisibleBy3(int p)
+        {
+            return p % 3 == 0;
+        }
+
+
     }
 }
+
--- FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
+++ FizzBuzzKata/FizzBuzzKataTests/FizzBuzzTest.cs
@@ -19,6 +19,59 @@ namespace FizzBuzzKataTests
             fizzBuzz = new FizzBuzz();
         }
 
-        
+        [Test]
+        public void InputOfOneShouldReturn1()
+        {
+            var result = fizzBuzz.GetFizzBuzz(1);
+            Assert.AreEqual("1", result);
+        }
+
+        [Test]
+        public void InputOfTwoShouldReturn2()
+        {
+            var result = fizzBuzz.GetFizzBuzz(2);
+            Assert.AreEqual("2", result);
+        }
+
+        [Test]
+        public void InputOfThreeShouldReturnFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(3);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void InputOfFiveShouldReturnBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(5);
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [Test]
+        public void InputOfSixShouldReturnFizz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(6);
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [Test]
+        public void InputOfTenShouldReturnBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(10);
+            Assert.AreEqual("Buzz", result);
+        }
+        [Test]
+        public void InputOfFifteenShouldReturnFizzBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(15);
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [Test]
+        public void InputOfThirtyShouldReturnFizzBuzz()
+        {
+            var result = fizzBuzz.GetFizzBuzz(30);
+            Assert.AreEqual("FizzBuzz", result);
+        }
     }
 }
